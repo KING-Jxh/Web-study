b01357074345cbe2aa159c49b24a3f36988d9086
KING-Jin <1520050179@qq.com>
2021-07-15 18:07:42 +0800

    tijiao

---
+++ 7-14/代码/js/fanhui.js
@@ -0,0 +1,5 @@
+function fanhui(){
+    var fanhui1 ="返回指定的位置，ABCDEFG";
+    var fanhui2 =fanhui1.charAt(6);
+    document.write(fanhui2);
+}
---
+++ 7-14/代码/js/indexOf().js
@@ -0,0 +1,4 @@
+var str="A BCDEFG HIGK"
+document.write(str.indexOf("C") + "<br />");
+document.write(str.indexOf("B") + "<br />");
+document.write(str.indexOf("G",8));
