25efbfc988e651a077fe1c42adcc7c797023ed3c
KING-Jin <1520050179@qq.com>
2021-07-15 12:00:15 +0800

    大会员免费了

---
+++ 7-14/代码/js/string.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: Jxh 
+ * @Date: 2021-07-15 09:18:57 
+ * @Last Modified by: Jxh
+ * @Last Modified time: 2021-07-15 09:39:51
+ */
+function zifuchuan(){
+    var mystring = "I'm a big boy";
+    var mystringlong = mystring.length;
+    document.write(mystringlong);
+}
+function zifuchuan1(){
+    var mystring1 = "kan wo shan lian gbia nshe n";
+    var mystring2 = mystring1.toUpperCase();
+    document.write(mystring2);
+}
